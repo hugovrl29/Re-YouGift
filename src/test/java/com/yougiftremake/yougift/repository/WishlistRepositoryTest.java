19691945cc4abb4fff62b436027612e82ff31a0e
Hugo Verly <47540879+hugovrl29@users.noreply.github.com>
2025-12-01 16:59:25 +0100

    Tests cleaning

--- src/test/java/com/yougiftremake/yougift/repository/WishlistRepositoryTest.java
+++ src/test/java/com/yougiftremake/yougift/repository/WishlistRepositoryTest.java
@@ -1,18 +1,11 @@
 package com.yougiftremake.yougift.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
